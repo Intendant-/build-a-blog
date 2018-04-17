ec3f6436a1f728333b9d77dff81ab3b9ad34c53d
brandoff <bperez83525@gmail.com>
2018-04-16 20:12:44 -0500

    there we go

---
+++ main.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.String(1000))
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, body, pub_date = None):
+        self.title = title
+        self.body = body
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
+
+def blog_entries():
+    return Blog.query.all()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    id = request.args.get('id')
+    if id:
+        post = Blog.query.filter_by(id=id).first()
+        return render_template('post.html', post = post)
+    else:
+        return render_template('blog.html', blog_entries = blog_entries()) ## blog_entries
+
+@app.route('/newpost/', methods=['GET', 'POST'])
+def newpost():
+
+    if request.method == 'POST':
+        title = request.form['blog_title']
+        body = request.form['blog_text']
+
+        # TODO validate form title and body
+        entry = Blog(title = title, body = body)
+        db.session.add(entry)
+        db.session.commit()
+        return redirect('/') 
+        ##return redirect('/?id=' + entry.id .format(entry))
+    else:
+        return render_template('newpost.html')
+
+
+if __name__ == "__main__":
+    app.run()        
