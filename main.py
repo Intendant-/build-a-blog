bff7287e34ce1598ce3314e992363fb2fd9cc2b7
brandoff <bperez83525@gmail.com>
2018-04-19 19:00:19 -0500

    finished

--- main.py
+++ main.py
@@ -33,18 +33,23 @@ def index():
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def newpost():
-
     if request.method == 'POST':
         title = request.form['blog_title']
         body = request.form['blog_text']
-
+        blg_title = ''
+        blg_text = ''
+        if not title or not body:
+            error = '**Please fill in both fields'
+            blg_title = title
+            blg_text = body
+            return render_template('newpost.html', blog_title = blg_title, blog_text = blg_text, error= error)
         # TODO validate form title and body
-        entry = Blog(title, body)
-        db.session.add(entry)
-        db.session.commit()
-        return render_template('test.html', entry = entry) ##redirect('/')
-        ##
-    else:
+        else:
+            entry = Blog(title, body)
+            db.session.add(entry)
+            db.session.commit()
+            return redirect('/blog?id={0}'.format(entry.id)) 
+    else:  
         return render_template('newpost.html')
 
 @app.route('/blog', methods=['GET', 'POST'])
@@ -56,14 +61,10 @@ def blog():
     else:
         return render_template('blog.html', blog_entries = blog_entries()) 
 
-##@app.route("blog?id=<integer:id>")
-
+@app.route("/blog?id=")
+def post():
+    id = request.args.get('id')
+    return render_template('post.html', entry=entry)
 
 if __name__ == "__main__":
     app.run()      
-
-
-
-## left TODO:
-#       validation of form data
-#       redirect to specific blog after submitting    
